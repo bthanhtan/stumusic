e55a7f128ce4f0055b62a81141bf57b30b20337b
bthanhtan <bthanhtan91@gmail.com>
2019-10-24 23:26:39 +0700

    a

--- stumusic/resources/views/listener/index.blade.php
+++ stumusic/resources/views/listener/index.blade.php
@@ -3,7 +3,31 @@
 @section('title', 'index')
 
 @section('main_content')
-	main_content
+	<form>
+		<select class="custom-select" id="select_1" onchange="tong()">
+		    <option selected>Choose...</option>
+		    <option value="1">One</option>
+		    <option value="2">Two</option>
+		    <option value="3">Three</option>
+		  </select>
+		<label>+</label>
+		<select class="custom-select" id="select_2" onchange="tong()">
+		    <option selected>Choose...</option>
+		    <option value="1">One</option>
+		    <option value="2">Two</option>
+		    <option value="3">Three</option>
+		  </select>
+		<label>-</label>
+		<select class="custom-select" id="select_3" onchange="tong()">
+		    <option selected>Choose...</option>
+		    <option value="1">One</option>
+		    <option value="2">Two</option>
+		    <option value="3">Three</option>
+		  </select>
+		<label>=</label>
+		<p id="ketqua">0</p>
+		<input type="button" class="button" value="tinh" onclick="return tong()">
+	</form>
 @stop
 
 
